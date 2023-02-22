89c73e11a7e75f16514c820beeaede7885db3ae8
hqdem <ilyadem4@gmail.com>
2023-02-22 21:49:24 +0300

    add method to download files from repos by their extension

--- githib_api/github_client.py
+++ githib_api/github_client.py
@@ -1,3 +1,4 @@
+import os
 from github import Github
 
 from config import Config
@@ -9,3 +10,25 @@ class GithubClient:
 
     def search_repos_by_language(self, lang):
         return self._client.search_repositories(query=f'language:{lang}')
+
+    def download_files_by_ext(self, repos, ext, save_path):
+        for repo in repos:
+            repo_name = repo.full_name.replace('/', '__')
+            if not os.path.exists(f'{save_path}/{repo_name}'):
+                os.mkdir(f'{save_path}/{repo_name}')
+            else:
+                continue
+            contents = repo.get_contents("")
+            while contents:
+                file_content = contents.pop(0)
+                if file_content.type == "dir":
+                    contents.extend(repo.get_contents(file_content.path))
+                else:
+                    print(f'Processing {file_content}')
+                    if file_content.name.endswith(ext):
+                        with open(f'{save_path}/{repo_name}/{file_content.name}', 'wb') as f:
+                            try:
+                                f.write(file_content.decoded_content)
+                            except AssertionError:
+                                print(f'Unable to write {file_content.name}. No encoding specified.')
+
