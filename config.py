f883572ce372af0518483355161359fe5828b2a5
hqdem <ilyadem4@gmail.com>
2023-02-23 12:44:50 +0300

    change db uri

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ from dataclasses import dataclass
 @dataclass
 class Config:
     github_access_token: str = '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'
-    db_uri: str = 'sqlite:///..\\data.db'
+    db_uri: str = 'sqlite:///data.db'
