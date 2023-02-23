f0724b73a2cb360e6020664e9ec457e6ff669891
hqdem <ilyadem4@gmail.com>
2023-02-23 16:10:55 +0300

    change logic of chunkify_search_results. now it optimised for every search_results length

--- githib_api/utils.py
+++ githib_api/utils.py
@@ -1,3 +1,6 @@
-def chunkify_search_results(results, size):
-    for i in range(0, results.totalCount, size):
-        yield results[i:i + size]
+def chunkify_search_results(results):
+    chunk_size = results.totalCount // 10  # get chunk_size for 10 workers
+    if chunk_size == 0:
+        chunk_size = 1
+    for i in range(0, results.totalCount, chunk_size):
+        yield results[i:i + chunk_size]
