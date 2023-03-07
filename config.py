5aacc1ef2749f50658443f94a11beb2053a7ab33
hqdem <107410043+hqdem@users.noreply.github.com>
2023-03-07 21:00:46 +0300

    delete token from config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    github_access_token: str = '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'
+    github_access_token: str = 'token here'
     db_uri: str = 'sqlite:///data.db'
